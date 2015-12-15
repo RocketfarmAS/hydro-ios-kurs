9d2c1ad2b2dde28689216760ffed2b59fd535ee0
Kostiantyn Koval <konstantin.koval1@gmail.com>
2015-12-15 10:54:07 +0100

    add example apps

---
+++ Day 1/SImple UI/Simple UI/Simple UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple UI
+//
+//  Created by Kostiantyn Koval on 14/12/15.
+//  Copyright © 2015 Kostiantyn Koval. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
